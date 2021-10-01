435e1c78a028515a5c9ee9a167aac5cac6e9bad0
Tapel <jaymel.tapel@accenture.com>
2021-10-01 08:32:24 +0800

    removed other  ts files before starting implmenting unit testings and some minor css fixes

--- src/app/core/services/customer.service.spec.ts
+++ src/app/core/services/customer.service.spec.ts
@@ -5,12 +5,5 @@ import { CustomerService } from './customer.service';
 describe('CustomerService', () => {
   let service: CustomerService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CustomerService);
-  });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+  
 });
